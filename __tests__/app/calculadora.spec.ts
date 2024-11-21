f3ea0e91c7d2e8014be2a28ffec070d95e092c1d
adrovez <adrovez@gmail.com>
2024-11-20 21:17:22 -0300

    aaaa

--- __tests__/app/calculadora.spec.ts
+++ __tests__/app/calculadora.spec.ts
@@ -1,5 +1,131 @@
 import { describe, test, expect } from "@jest/globals";
-import { suma, restar, operar } from "../../src/app/calculadora";
+import { suma, operar, restar, multiplicar, dividir, potencia, factorial } from "../../src/app/calculadora";
+
+
+describe("Bateria de test de calculadora", () => {
+
+    describe("Pruebas de Suma", () => {
+        test("Suma de números válidos", () => {
+            expect(suma(3, 2)).toBe(5);
+        });
+
+        test("Suma con valores indefinidos", () => {
+            expect(() => suma(undefined as unknown as number, 2)).toThrow("No se puede sumar indefinidos");
+        });
+
+        test("Suma con valores no numéricos", () => {
+            expect(suma("a" as unknown as number, 2)).toBeNaN();
+        });
+    });
+
+    describe("Pruebas de Resta", () => {
+        test("Resta de números válidos", () => {
+            expect(restar(5, 2)).toBe(3);
+        });
+
+        test("Resta con valores indefinidos", () => {
+            expect(() => restar(undefined as unknown as number, 2)).toThrow("No se puede restar indefinidos");
+        });
+
+        test("Resta con valores no numéricos", () => {
+            expect(restar("a" as unknown as number, 2)).toBeNaN();
+        });
+    });
+
+    describe("Pruebas de Multiplicación", () => {
+        test("Multiplicación de números válidos", () => {
+            expect(multiplicar(3, 2)).toBe(6);
+        });
+
+        test("Multiplicación con valores indefinidos", () => {
+            expect(() => multiplicar(undefined as unknown as number, 2)).toThrow("No se puede multiplicar indefinidos");
+        });
+
+        test("Multiplicación con valores no numéricos", () => {
+            expect(multiplicar("a" as unknown as number, 2)).toBeNaN();
+        });
+    });
+
+    describe("Pruebas de División", () => {
+        test("División de números válidos", () => {
+            expect(dividir(6, 2)).toBe(3);
+        });
+
+        test("División por cero", () => {
+            expect(dividir(6, 0)).toBe(Infinity);
+        });
+
+        test("División con valores indefinidos", () => {
+            expect(() => dividir(undefined as unknown as number, 2)).toThrow("No se puede dividir indefinidos");
+        });
+
+        test("División con valores no numéricos", () => {
+            expect(dividir("a" as unknown as number, 2)).toBeNaN();
+        });
+    });
+
+    describe("Pruebas de Potencia", () => {
+        test("Potencia de números válidos", () => {
+            expect(potencia(2, 3)).toBe(8);
+        });
+
+        test("Potencia con valores indefinidos", () => {
+            expect(() => potencia(undefined as unknown as number, 2)).toThrow("No se puede dividir indefinidos");
+        });
+
+        test("Potencia con valores no numéricos", () => {
+            expect(potencia("a" as unknown as number, 2)).toBeNaN();
+        });
+    });
+
+    describe("Pruebas de Factor", () => {
+        test("Factorial de un número válido", () => {
+            expect(factorial(5)).toBe(120);
+        });
+
+        test("Factorial de 0", () => {
+            expect(factorial(0)).toBe(1);
+        });
+
+        test("Factorial de un número negativo", () => {
+            expect(() => factorial(-1)).toThrow("El número debe ser mayor o igual a 0.");
+        });
+    });
+
+    describe("Operar", () => {
+        test("Operación suma válida", () => {
+            expect(operar("suma", 4, 3)).toBe(7);
+        });
+
+        test("Operación resta válida", () => {
+            expect(operar("resta", 7, 4)).toBe(3);
+        });
+
+        test("Operación multiplicación válida", () => {
+            expect(operar("multiplica", 3, 3)).toBe(9);
+        });
+
+        test("Operación división válida", () => {
+            expect(operar("divide", 8, 2)).toBe(4);
+        });
+
+        test("Operación potencia válida", () => {
+            expect(operar("potencia", 2, 4)).toBe(16);
+        });
+
+        test("Operación factorial válida", () => {
+            expect(operar("factor", 5, 0)).toBe(120);
+        });
+
+        test("Operación desconocida", () => {
+            expect(operar("desconocida", 4, 3)).toBeUndefined();
+        });
+    });
+});
+
+/*
+
+//import { suma, restar, operar } from "../../src/app/calculadora";
 
 describe("Bateria de test de calculadora", () => {
 
@@ -70,3 +196,5 @@ describe("Bateria de test de calculadora", () => {
     });
 
 });
+
+*/
