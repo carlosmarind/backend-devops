a7d185e8de1abfe16f6b33a65b55372d54200b0c
Jorge <jelizalde316@DESKTOP-1C71JE7.>
2025-04-20 19:13:59 -0400

    Laboratorio 2

--- src/app/calculadora.ts
+++ src/app/calculadora.ts
@@ -7,6 +7,10 @@ function operar(operacion: string = "", a: number, b: number) {
         return multiplica(a, b);
     }else if (operacion === 'divide') {
         return divide(a, b);
+    }else if (operacion === 'potencia') {
+        return potencia(a, b);
+    }else if (operacion === 'factorial') {
+        return factorial(a);
     }
 }
 
@@ -32,7 +36,7 @@ function restar(a: number, b: number) {
 
 function multiplica(a: number, b: number) {
     if (a === undefined || b === undefined) {
-        throw new Error("No se puede restar indefinidos");
+        throw new Error("No se puede multiplicar indefinidos");
     }
     if (typeof a !== 'number' || typeof b !== 'number') {
         return NaN;
@@ -42,11 +46,37 @@ function multiplica(a: number, b: number) {
 
 function divide(a: number, b: number) {
     if (a === undefined || b === undefined) {
+        throw new Error("No se puede dividir indefinidos");
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return NaN;
+    }
+    return (a/b);
+}
+
+function potencia(a: number, b: number) {
+    if (a === undefined || b === undefined) {
         throw new Error("No se puede restar indefinidos");
     }
     if (typeof a !== 'number' || typeof b !== 'number') {
         return NaN;
     }
-    return a / b;
+    return Math.pow(a, b);
 }
-export { suma, operar, restar };
+
+function factorial(a: number) {
+    if (a === undefined) {
+        throw new Error("No se puede potencia de indefinidos");
+    }
+    if (typeof a !== 'number') {
+        return NaN;
+    }
+
+    var num=1;
+    for (var i = 1; i <= a; i++) {
+        num = num * i;
+    }
+    return num;
+}
+
+export { suma, operar, restar, multiplica, divide, potencia, factorial };
