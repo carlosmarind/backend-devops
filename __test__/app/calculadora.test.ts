981258176dee09e7158b366b0c22c73b721b5553
Jorge <jelizalde316@DESKTOP-1C71JE7.>
2025-04-21 17:17:53 -0400

    laboratorio 2

--- __test__/app/calculadora.test.ts
+++ __test__/app/calculadora.test.ts
@@ -1,20 +1,20 @@
 import { describe, test } from "@jest/globals";
-import {restar, suma, multiplica, divide, potencia, factorial} from "../../src/app/calculadora";
+import {operar, restar, suma, multiplica, divide, potencia, factorial} from "../../src/app/calculadora";
+import { isUndefined } from "util";
 
 describe("suite de test de calculadora", () => {
 
     test("probar suma de enteros", () => {
-        let a: any = "10";
+        let a: any = undefined;
         expect(suma(1,1)).toBe(2);
         expect(suma(10,15)).toBe(25);
         expect(suma(a,15)).toBeNaN();
     });
 
-    test("probar resta de enteros", () => {
-        let a: any = "10";
-        expect(restar(10,5)).toBe(5);
+    test("probar resta de enteros", () => {          
+        let a: any = undefined;
         expect(restar(1,1)).toBe(0);
-        expect(restar(1,10)).toBe(-9);
+        expect(restar(15,10)).toBe(5);
         expect(restar(a,15)).toBeNaN();
     });
 
@@ -27,27 +27,34 @@ describe("suite de test de calculadora", () => {
     });
 
     test("probar divición de enteros", () => {
-        let a: any = "10";
-        //expect(divide(10,5)).toBe(2);
+        let a: any = undefined;
         expect(divide(1,1)).toBe(1);
         expect(divide(10,10)).toBe(1);
-        //expect(divide(a,15)).toBeNaN();
-        //expect(divide(0,0)).toBe("No se puede dividir el número 0");
+        expect(divide(a,15)).toBeNaN();
     });
 
     test("probar potencia de enteros", () => {
-        let a: any = "10";
+        let a: any = undefined;
         expect(potencia(10,5)).toBe(100000);
         expect(potencia(1,1)).toBe(1);
         expect(potencia(1,10)).toBe(1);
         expect(potencia(a,15)).toBeNaN();
     });
     test("probar factorial de un número", () => {
-        let a: any = "10";
+        let a: any = undefined;
         expect(factorial(5)).toBe(120);
         expect(factorial(4)).toBe(24);
         expect(factorial(6)).toBe(720);
-        //expect(factorial(a)).toBeNaN();
+        expect(factorial(a)).toBeNaN();
     });
 
+    test("probar operar de un número", () => {
+        let a: any = undefined;
+        expect(operar("suma",1,2)).toBe(3);
+        expect(operar("factorial",5,0)).toBe(120);
+        expect(operar("resta",2,2)).toBe(0);
+        expect(operar("multiplica",1,2)).toBe(2);
+        expect(operar("divide",1,1)).toBe(1);
+        expect(operar("potencia",10,5)).toBe(100000);
+    });
 });
